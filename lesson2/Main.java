b68db0aef9455199c6b8465aaa249633c8768623
fimakoff <fimakoff@gmail.com>
2019-05-27 16:42:23 +0300

    easy ref

--- lesson2/Main.java
+++ lesson2/Main.java
@@ -1,6 +1,7 @@
 package lesson2;
 
 public class Main {
+    private static final int countOfElements = 4;
     public static void main(String[] args) {
         String[][] array1 = new String[][]{
                 {"0", "1", "2", "3"},
@@ -65,9 +66,9 @@ public class Main {
 
     private static void parseAndSumm(String[][] arr) throws MyArraySizeException, MyArrayDataException {
         int summ = 0;
-        if (arr.length != 4) throw new MyArraySizeException("Размер внешнего массива не соответствует необходимому");
+        if (arr.length != countOfElements) throw new MyArraySizeException("Размер внешнего массива не соответствует необходимому");
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i].length != 4)
+            if (arr[i].length != countOfElements)
                 throw new MyArraySizeException("Размер внутреннего массива не соответствует необходимому");
             for (int j = 0; j < arr[i].length; j++) {
                 if (!isNumeric(arr[i][j])) throw new MyArrayDataException("Неверный тип данных в ячейке ", i, j);
