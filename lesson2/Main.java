2b73646a81514e5a5c455aca4991b2fa9ef762e8
fimakoff <fimakoff@gmail.com>
2019-05-28 07:23:12 +0300

    Start work!

--- lesson2/Main.java
+++ lesson2/Main.java
@@ -3,27 +3,27 @@ package lesson2;
 public class Main {
     private static final int countOfElements = 4;
     public static void main(String[] args) {
-        String[][] array1 = new String[][]{
-                {"0", "1", "2", "3"},
-                {"4", "5", "6", "7"},
-                {"8", "9", "10", "11"},
-                {"10", "12", "13", "14", "15"}
-        };
-
-        String[][] array2 = new String[][]{
-                {"0", "1", "2", "3"},
-                {"4", "5", "6", "7"},
-                {"8", "9", "10", "11"},
-                {"12", "13", "14", "15"},
-                {}
-        };
-
-        String[][] array3 = new String[][]{
-                {"0", "1", "2", "3"},
-                {"4", "5", "6", "7"},
-                {"8", "STRING", "10", "11"},
-                {"12", "13", "14", "15"}
-        };
+//        String[][] array1 = new String[][]{
+//                {"0", "1", "2", "3"},
+//                {"4", "5", "6", "7"},
+//                {"8", "9", "10", "11"},
+//                {"10", "12", "13", "14", "15"}
+//        };
+//
+//        String[][] array2 = new String[][]{
+//                {"0", "1", "2", "3"},
+//                {"4", "5", "6", "7"},
+//                {"8", "9", "10", "11"},
+//                {"12", "13", "14", "15"},
+//                {}
+//        };
+//
+//        String[][] array3 = new String[][]{
+//                {"0", "1", "2", "3"},
+//                {"4", "5", "6", "7"},
+//                {"8", "STRING", "10", "11"},
+//                {"12", "13", "14", "15"}
+//        };
 
         String[][] array4 = new String[][]{
                 {"0", "1", "2", "3"},
@@ -31,29 +31,29 @@ public class Main {
                 {"8", "9", "10", "11"},
                 {"12", "13", "14", "15"}
         };
-        try {
-            parseAndSumm(array1);
-        } catch (MyArraySizeException e) {
-            System.out.println(e.getMessage());
-        } catch (MyArrayDataException e) {
-            e.getMessage();
-        }
-
-        try {
-            parseAndSumm(array2);
-        } catch (MyArraySizeException e) {
-            System.out.println(e.getMessage());
-        } catch (MyArrayDataException e) {
-            e.getMessage();
-        }
-
-        try {
-            parseAndSumm(array3);
-        } catch (MyArraySizeException e) {
-            System.out.println(e.getMessage());
-        } catch (MyArrayDataException e) {
-            e.getMessage();
-        }
+//        try {
+//            parseAndSumm(array1);
+//        } catch (MyArraySizeException e) {
+//            System.out.println(e.getMessage());
+//        } catch (MyArrayDataException e) {
+//            e.getMessage();
+//        }
+//
+//        try {
+//            parseAndSumm(array2);
+//        } catch (MyArraySizeException e) {
+//            System.out.println(e.getMessage());
+//        } catch (MyArrayDataException e) {
+//            e.getMessage();
+//        }
+//
+//        try {
+//            parseAndSumm(array3);
+//        } catch (MyArraySizeException e) {
+//            System.out.println(e.getMessage());
+//        } catch (MyArrayDataException e) {
+//            e.getMessage();
+//        }
 
         try {
             parseAndSumm(array4);
