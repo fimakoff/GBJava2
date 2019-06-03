314822292fb02d96e7ac37150172f7dc5335f5a2
fimakoff <fimakoff@gmail.com>
2019-06-03 16:43:12 +0300

    Доработки задания №1.

--- lesson3/HW/Main.java
+++ lesson3/HW/Main.java
@@ -1,31 +1,40 @@
 package lesson3.HW;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         // Задание №1
-        ex1();
+        String[] arr = {"1", "2", "3", "4", "5", "6", "7", "3", "8", "9", "10", "11", "12", "13", "14", "15", "16", "16"};
+        ex1a(arr);
+        ex1b(arr);
         // Задание №2
         ex2();
     }
 
-    private static void ex1() {
-        String[] arr = {"1", "2", "3", "4", "5", "6", "7", "3", "8", "9", "10", "11", "12", "13", "14", "15", "16", "16"};
+    private static void ex1b(String[] arr) {
+        System.out.println("\nКоличество повторений элементов массива:");
+        Map<String, Integer> words = new HashMap<>();
+        ArrayList<String> inArray = new ArrayList<>(Arrays.asList(arr));
+
+        for (String tempWord : inArray)
+            if (!words.containsKey(tempWord)) words.put(tempWord, 1);
+            else words.put(tempWord, words.get(tempWord) + 1);
+
+        for (Map.Entry<String, Integer> entry : words.entrySet())
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+    }
+
+    private static void ex1a(String[] arr) {
+        System.out.println("\nЗадание №1.");
         List<String> inArray = new ArrayList<>(Arrays.asList(arr));
-        List<String> outArray = new ArrayList<>();
-        for (String str : inArray) {
-            if ((inArray.indexOf(str) == inArray.indexOf(str)) && !outArray.contains(str)) {
-                outArray.add(str);
-            }
-        }
+        Set<String> outArray = new HashSet<>(inArray);
         System.out.println(inArray);
         System.out.println(outArray);
     }
 
     private static void ex2() {
+        System.out.println("\nЗадание №2.");
         PhoneBook phoneBook = new PhoneBook();
         phoneBook.add("Иванов", "+71231234567");
         phoneBook.add("Иванов", "+72342345678");
