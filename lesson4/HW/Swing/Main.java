a4102b17bb50b326c2df9ef67f6109fc1cc3ce35
fimakoff <fimakoff@gmail.com>
2019-06-05 13:07:06 +0300

    Добавил чат, реализованный при помощи Java FX. Мелкие правки в предыдущем чате.

--- lesson4/HW/Swing/Main.java
+++ lesson4/HW/Swing/Main.java
@@ -3,6 +3,5 @@ package lesson4.HW.Swing;
 public class Main {
     public static void main(String[] args) {
         new MyWindow();
-
     }
 }
--- lesson4/HW/Swing/MyWindow.java
+++ lesson4/HW/Swing/MyWindow.java
@@ -2,8 +2,6 @@ package lesson4.HW.Swing;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 
